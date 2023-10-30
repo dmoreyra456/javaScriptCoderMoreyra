67f671ae635445fdade2ceee186c3259dbdd6904
Daniel Moreyra <escepticodada@gmail.com>
2023-10-29 22:12:05 -0300

    preentrega2Moreyra

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const Goku = {
+    nombre: "Goku",
+    poder: 8000
+}
+
+const Vegetta = {
+    nombre: "Vegetta",
+    poder: 18000
+}
+
+const Piccoro = {
+    nombre: "Piccoro",
+    poder: 1220
+}
+
+const listaDeGuerreros = [Goku, Vegetta, Piccoro];
+
 function buscarElSuperGuerrero(listaDePersonas) {
     var masPoderoso = listaDePersonas[0];
 
@@ -6,28 +23,51 @@ function buscarElSuperGuerrero(listaDePersonas) {
         if (persona.poder > masPoderoso.poder) {
             masPoderoso = persona;
         }
+    }
+
+    return masPoderoso;
 }
 
-return masPoderoso;
+function buscarGuerrero() {
+    const peleador = document.getElementById("peleador");
+    var peleadorElegido = {
+        nombre: "Piccoro",
+        poder: ""
+    };
+    var existe = false;
+    for (let index = 0; index < listaDeGuerreros.length; index++) {
+        if (listaDeGuerreros[index].nombre == peleador.value) {
+            existe = true;
+            peleadorElegido = listaDeGuerreros[index];
+            break;
+        }
+    }
+
+    var card = document.getElementById("tarjetaGuerrero");
+    if (existe) {
+        var newHtml = `
+        <div class = "tarjeta">
+            <h2>Guerrero</h2>
+            <div>Nombre: ${peleadorElegido.nombre} </div>
+            <div>Poder: ${peleadorElegido.poder} </div>
+        </div>
+        `;
+        card.innerHTML = newHtml;
+        console.log(card);
+
+    } else {
+        card.innerHTML = "No se encontró el guerrero querido Sayayin.";
+    }
 }
 
-const Goku = {
-nombre: "Goku",
-poder: 8000
+function agregarPeleador() {
+    var nombrePeleador = document.getElementById("nuevoNombre").value;
+    var poderPeleador = document.getElementById("nuevoPoder").value;
+
+    var nuevoGuerrero = {
+        nombre: nombrePeleador,
+        poder: parseFloat(poderPeleador)
+    }
+
+    listaDeGuerreros.push(nuevoGuerrero);
 }
-
-const Vegetta = {
-nombre: "Vegetta",
-poder: 18000
-}
-
-const Piccoro = {
-nombre: "Piccoro",
-poder: 1220
-}
-
-const guerrerosSaiyan = [Goku, Vegetta, Piccoro];
-
-var guerrero = buscarElSuperGuerrero(guerrerosSaiyan);
-
-console.log("El guerrero más poderoso es " + guerrero.nombre);
